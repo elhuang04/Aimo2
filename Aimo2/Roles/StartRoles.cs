8f91e19563a90bbe490ff2fb972f6bac22002dd1
Elizabeth Huang <elhuang04@gmail.com>
2023-01-21 16:46:58 -0500

    claim button changes + styles

---
+++ Aimo2/Roles/StartRoles.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aimo2.Roles
+{
+    public class StartRoles : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
